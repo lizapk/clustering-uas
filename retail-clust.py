4ded189801f52332824df6c576bb970ce478f176
lizapk <126601571+lizapk@users.noreply.github.com>
2023-11-08 05:44:12 +0700

    Update retail-clust.py

--- retail-clust.py
+++ retail-clust.py
@@ -4,45 +4,77 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.cluster import KMeans
 
-df = pd.read_excel('dataset_processed.xlsx')
-df = df.drop(['Unnamed: 0'],axis=1)
+# Load data
+df = pd.read_excel('processed_scale.xlsx')
+df = df.drop(['Unnamed: 0'], axis=1)
+
+df1 = pd.read_csv('OnlineRetail.csv', encoding='latin1')
+
+df2 = pd.read_excel('processed_nonscale.xlsx')
+df2 = df2.drop(['Unnamed: 0'], axis=1)
 
 # Header Interface
-st.header("Isi dataset")
-st.write(df)
+st.header("Dataset original")
+st.write(df1)
+
+st.header("Isi dataset yang sudah diproses")
+st.write(df2)
 
 # Create the slider for selecting the number of clusters (K)
 st.sidebar.subheader("Nilai jumlah K")
-clust = st.sidebar.slider("Pilih jumlah cluster: ", 2, 10, 1, 1)
+n_clust = st.sidebar.slider("Pilih jumlah cluster: ", 2, 4, 2, 1)
 
-# Scatter plot function
-def k_means(n_clust):
-    kmean = KMeans(n_clusters=n_clust).fit(df)
+# Create a selection box for scatter plot type
+selected_plot = st.selectbox("Pilih Visualisasi Klasterisasi Data:", 
+                             ["Total Belanja dan Terakhir Kali Belanja", 
+                              "Total Belanja dan Frekuensi Belanja", 
+                              "Total Belanja, Frekuensi Belanja dan Terakhir Kali Belanja"])
+
+# Scatter plot functions
+def plot_amount_vs_recency(n_clust):
+    kmean = KMeans(n_clusters=n_clust, n_init=10).fit(df)
     df['Labels'] = kmean.labels_
 
-    st.subheader('Cluster Plot')
-    fig, axes = plt.subplots(3,1, figsize=(20, 40))
+    st.subheader('Total Belanja dan Terakhir Kali Belanja')
+    fig, ax = plt.subplots(figsize=(10, 5))
 
-    sns.scatterplot(x=df['Amount'], y=df['Recency'], hue=df['Labels'], palette=sns.color_palette('hls', n_colors=n_clust), ax=axes[0])
-    axes[0].set_title('Amount vs Recency')
+    sns.scatterplot(x=df['Amount'], y=df['Recency'], hue=df['Labels'], palette=sns.color_palette('hls', n_colors=n_clust), ax=ax)
+    st.pyplot(fig)
 
-    sns.scatterplot(x=df['Amount'], y=df['Frequency'], hue=df['Labels'], palette=sns.color_palette('hls', n_colors=n_clust), ax=axes[1])
-    axes[1].set_title('Amount vs Frequency')
+def plot_amount_vs_frequency(n_clust):
+    kmean = KMeans(n_clusters=n_clust, n_init=10).fit(df)
+    df['Labels'] = kmean.labels_
 
-    sns.scatterplot(x=df['Amount'], y=df['Frequency'], hue=df['Recency'], ax=axes[2])
-    axes[2].set_title('Amount vs Frequency vs Recency')
+    st.subheader('Total Belanja dan Frekuensi Belanja')
+    fig, ax = plt.subplots(figsize=(10, 5))
 
+    sns.scatterplot(x=df['Amount'], y=df['Frequency'], hue=df['Labels'], palette=sns.color_palette('hls', n_colors=n_clust), ax=ax)
+    st.pyplot(fig)
+
+def plot_amount_vs_frequency_vs_recency(n_clust):
+    kmean = KMeans(n_clusters=n_clust, n_init=10).fit(df)
+    df['Labels'] = kmean.labels_
+
+    st.subheader('Total Belanja, Frekuensi Belanja dan Terakhir Kali Belanja')
+    fig, ax = plt.subplots(figsize=(10, 5))
+
+    sns.scatterplot(x=df['Amount'], y=df['Frequency'], hue=df['Recency'], ax=ax)
     st.pyplot(fig)
 
 # Clustering process and scatter plot
 if st.button("Proses Klasterisasi dan Tampilkan Plot"):
     clusters = []
     for i in range(1, 11):
-        km = KMeans(n_clusters=i).fit(df)
+        km = KMeans(n_clusters=i, n_init=10).fit(df)
         clusters.append(km.inertia_)
 
     # Display the elbow plot
     st.subheader("Mencari Elbow")
     st.line_chart(clusters, use_container_width=True)
 
-    k_means(clust)
+    if selected_plot == "Total Belanja dan Terakhir Kali Belanja":
+        plot_amount_vs_recency(n_clust)
+    elif selected_plot == "Total Belanja dan Frekuensi Belanja":
+        plot_amount_vs_frequency(n_clust)
+    elif selected_plot == "Total Belanja, Frekuensi Belanja dan Terakhir Kali Belanja":
+        plot_amount_vs_frequency_vs_recency(n_clust)
